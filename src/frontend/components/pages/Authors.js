bb677f3f5aaaedc441d8c7a67f56675cec822fe0
John Watson <johnwatson484@hotmail.com>
2020-08-27 18:14:38 +0100

    Async data added

--- src/frontend/components/pages/Authors.js
+++ src/frontend/components/pages/Authors.js
@@ -2,27 +2,59 @@ import react from 'react'
 import htm from 'htm'
 import { FourOhFour } from './FourOhFour.js'
 import { Header } from '../Header.js'
-import { authors } from '../../../data/authors.js'
+import superagent from 'superagent'
 
 const html = htm.bind(react.createElement)
 
 export class Author extends react.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      author: null,
+      loading: true
+    }
+  }
+
+  async loadData () {
+    let author = null
+    this.setState({ loading: false, author })
+    try {
+      const { body } = await superagent.get(
+        `http://127.0.0.1:3001/api/author/${this.props.match.params.authorId}`)
+      author = body
+    } catch (e) {}
+    this.setState({ loading: false, author })
+  }
+
+  componentDidMount () {
+    this.loadData()
+  }
+
+  componentDidUpdate (prevProps) {
+    if (prevProps.match.params.authorId !==
+      this.props.match.params.authorId) {
+      this.loadData()
+    }
+  }
+
   render () {
-    const author = authors.find(
-      author => author.id === this.props.match.params.authorId
-    )
-    if (!author) {
+    if (this.state.loading) {
+      return html`<${Header}/><div>Loading ...</div>`
+    }
+
+    if (!this.state.author) {
       return html`<${FourOhFour} 
         staticContext=${this.props.staticContext}
         error="Author not found"/>`
     }
+
     return html`<div>
       <${Header}/>
-      <h2>${author.name}</h2>
-      <p>${author.bio}</p>
+      <h2>${this.state.author.name}</h2>
+      <p>${this.state.author.bio}</p>
       <h3>Books</h3>
       <ul>
-        ${author.books.map((book) =>
+        ${this.state.author.books.map((book) =>
           html`<li key=${book.id}>${book.title} (${book.year})</li>`
         )}
       </ul>
